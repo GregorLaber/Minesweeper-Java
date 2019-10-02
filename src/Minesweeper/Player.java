a45b317a3a3327a7f2e901d6ee4e09bdc4980c50
Gregor <fatAdmiralTot@users.noreply.github.com>
2019-10-02 19:42:04 +0200

    feat: Highscore implementieren
    - added new class player for highscore

---
+++ src/Minesweeper/Player.java
@@ -0,0 +1,38 @@
+package Minesweeper;
+
+class Player {
+
+    private int rank;
+    private String name;
+    private String time;
+
+    public Player(int rank, String name, String time) {
+        this.rank = rank;
+        this.name = name;
+        this.time = time;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
